a7536ce485409bdc92cbb7df8727adfc4ca27685
Moqr134 <mohamadqasm68@gmail.com>
2026-03-01 04:07:43 +0300

    Add files via upload

--- DemoWAS/Program.cs
+++ DemoWAS/Program.cs
@@ -1,19 +1,22 @@
 ﻿using DemoWAS;
-using DemoWAS.DTO;
 using DemoWAS.Service;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddSingleton<CartService>();
+builder.Services.AddSingleton<UserInfoService>();
 builder.Services.AddScoped<IItemService, ItemService>();
+builder.Services.AddScoped<IBillsService, BillsService>();
+builder.Services.AddScoped<INotifSrvice, NotifSrvice>();
+builder.Services.AddScoped<ISizeService, SizeService>();
 builder.Services.AddScoped(sp =>
     new HttpClient
     {
-        BaseAddress = new Uri("https://localhost:7292/")
+        BaseAddress = new Uri("https://api.alfakhani.com/")
+        //BaseAddress = new Uri("https://localhost:7292/")
     });
 await builder.Build().RunAsync();
